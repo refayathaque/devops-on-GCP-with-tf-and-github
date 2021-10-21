560709b4e128462b20df908c731e94a7e6f78b0d
Refayat Haque <refayathaque@gmail.com>
2021-10-20 23:47:56 -0400

    slup

--- react-demo-1/node-scripts/storage.js
+++ react-demo-1/node-scripts/storage.js
@@ -1,19 +1,18 @@
-import vars from "./vars.js";
 // https://stackoverflow.com/a/68783000
 // So, on top of what other suggested of using "type": "module" on package.json you also need to specify the file extension import {urls} from './helpers.js'
+import vars from "./vars.js";
 import { Storage } from "@google-cloud/storage";
 
-function listBuckets() {
-  const { keyFilename } = vars;
-  const storage = new Storage({
-    keyFilename,
-  });
+const { keyFilename } = vars;
+const storage = new Storage({
+  keyFilename,
+});
 
-  async function listBuckets() {
+function listBucketsTransformed() {
+  async function fetchBuckets() {
     const response = await storage.getBuckets();
     const [buckets] = response;
     // response gives data we want in an array inside of another array [[]], the array we want has an index of 0, so we're destructuring that out and giving that array the name "buckets"
-
     buckets.forEach((bucket) => {
       // console.log(bucket.name);
       console.log(bucket.metadata);
@@ -21,10 +20,18 @@ function listBuckets() {
       // ^ to see everything that's in the Bucket object
     });
   }
-
-  listBuckets().catch(console.error);
+  fetchBuckets().catch(console.error);
 }
 
-listBuckets();
+function listBuckets() {
+  async function fetchBuckets() {
+    const response = await storage.getBuckets();
+    console.log(response);
+  }
+  fetchBuckets().catch(console.error);
+}
+
+listBucketsTransformed();
+// listBuckets();
 
 // https://github.com/googleapis/nodejs-storage/blob/main/samples/listBuckets.js
